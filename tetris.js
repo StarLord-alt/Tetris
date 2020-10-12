90d16d7c56310a5a01f7be41f7403b777c66d005
StarLorn-alt <levi.akkerman.storage@gmail.com>
2020-10-12 12:28:16 +0200

    Amelioration de la boucle temporelle + utilisation de callback pour la fonction draw

--- tetris.js
+++ tetris.js
@@ -3,7 +3,7 @@ let title=document.querySelector(".score")
 let grid_area=document.querySelector(".tetris")
 let grid=[];
 let block_size=50
-let OFFSET={x:2,y:6}
+let OFFSET={x:4,y:1}
 let brique;
 let temp;
 let identifian=1;
@@ -38,22 +38,72 @@ const FORME_L_P3=[{x:1,y:1},{x:2,y:1},{x:3,y:1},{x:1,y:2}]
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-window.onload=InitCanvas
+var width = canvas.width
+var height = canvas.height
+
+///////////////////////////////////////////////////////////////////////////////
+
+function update(callback) {
+    let index=getIndexElement(grid,brique)
+    if( index !== -1){
+
+        grid[index][2].offset.y++
+        callback()
+    }
+    // Update the state of the world for the elapsed time since last render
+}
+
+function loop(timestamp) {
+    var progress = timestamp - lastRender
+    if(progress>1000){
+        update(function(){
+            let offset_brique;
+            let x,y
+            let temp=[]
+            //ctx.clearRect(0, 0, width, height)
+            grid.forEach(function(element){
+                offset_brique=element[2].offset;
+                console.log(offset_brique.y)
+                element[1].forEach(function (e){
+                    x=(offset_brique.x + e.x)*block_size
+                    y=(offset_brique.y + e.y)*block_size
+                    ctx.rect(x,y,block_size, block_size);
+                    ctx.fillStyle = element[2].color;
+                    ctx.fill();
+                    ctx.lineWidth = 3;
+                    ctx.strokeStyle = "#000000";
+                    ctx.stroke();
+                    ctx.closePath()
+                })
+
+
+
+            });
+        })
+
+        lastRender = timestamp
+
+    }
+
+    window.requestAnimationFrame(loop)
+}
+InitCanvas()
+var lastRender = 0
+window.requestAnimationFrame(loop)
+
+
+//////////////////////////////////////////////
+
+
 
 
 function InitCanvas(){
     ctx.fillStyle = '#F9DDC4';
     ctx.fillRect(0, 0, 500, 700);
     brique= new createBrique(identifian,FORME_Z,OFFSET,FORME_Z[2].color)
-    console.log(brique)
     grid.push(brique)
     identifian++;
-    let brique2=FORME_Z;
-    brique2[0].id=identifian
-    grid.push(brique2)
-    console.log(brique2)
 
-   // GameLoop()
 
   //  setInterval(onTimerTick, 1000); // 33 milliseconds = ~ 30 frames per sec
 
@@ -62,35 +112,39 @@ function InitCanvas(){
 
 function createBrique(id,forme,offset_param,color){
     let newBrique=[]
-    newBrique[0].id = id;
+    newBrique[0] = {"id": id };
     newBrique[1]=FORME_Z[1];
-    newBrique[2].offset=offset_param;
-    newBrique[2].color=color;
+    newBrique[2]={"offset":offset_param};
+    newBrique[2].color=color
     return newBrique
 }
 
-function GameLoop() {
-    //boucle externe pour creation d'une nouvelle brique ici
-
-         let index=getIndexElement(grid,brique)
-            console.log(index)
-    console.log(grid)
-
-        // for(let i=0;i<100;i++){
-        //     setTimeout(function() {
-        //         if( index !== -1){
-        //             grid[index][1].forEach(function (e){
-        //                 e.y++
-        //
-        //             })
-        //         }else{
-        //            // grid.push(brique)
-        //         }
-        //         DrawGrid();
-        //     }, 500 * i);
-        // }
-
-}
+// function GameLoop() {
+//     //boucle externe pour creation d'une nouvelle brique ici
+//
+//          let index=getIndexElement(grid,brique)
+//
+//         for(let i=0;i<20;i++){
+//             setTimeout(function() {
+//                 //ctx.clearRect(0, 0, canvas.width, canvas.height);
+//                 if( index !== -1){
+//                     // grid[index][1].forEach(function (e){
+//                     //     e.y++
+//                     //
+//                     // })
+//                     DrawGrid();
+//                     grid[index][2].offset.y++
+//                     console.log(grid[index][2].offset.y)
+//                 }else{
+//                    // grid.push(brique)
+//                 }
+//
+//             }, 500 * i);
+//             console.log(grid)
+//         }
+//
+//
+// }
 function getIndexElement(grid,element){
     let trouve=false;
     for(let i=0;i<grid.length;i++){
@@ -106,28 +160,27 @@ function getIndexElement(grid,element){
     }
 
 }
-function DrawGrid(){
-    let offset;
-    let x,y
-
-
-
-    grid.forEach(function(element){
-        offset=element[2].offset;
-        element.forEach(function (e){
-            x=(offset.x + e.x)*block_size
-            y=(offset.y + e.y)*block_size
-
-            ctx.rect(x,y,block_size, block_size);
-            ctx.fillStyle = element[2].color;
-            ctx.fill();
-            ctx.lineWidth = 3;
-            ctx.strokeStyle = "#000000";
-            ctx.stroke();
-        })
-
-
-
-    });
-
-}
+// function draw(grid){
+//     let offset_brique;
+//     let x,y
+//     ctx.clearRect(0, 0, width, height)
+//     grid.forEach(function(element){
+//         offset_brique=element[2].offset;
+//         //console.log(element)
+//         element[1].forEach(function (e){
+//             x=(offset_brique.x + e.x)*block_size
+//             y=(offset_brique.y + e.y)*block_size
+//             ctx.rect(x,y,block_size, block_size);
+//             ctx.fillStyle = element[2].color;
+//             ctx.fill();
+//             ctx.lineWidth = 3;
+//             ctx.strokeStyle = "#000000";
+//             ctx.stroke();
+//             ctx.closePath()
+//         })
+//
+//
+//
+//     });
+//
+// }
